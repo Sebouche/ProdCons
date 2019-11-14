a5e452db6bbb0aa80a761eaef7eb5b2e0c83d5e9
Sebouche <sebastian71f@gmail.com>
2019-11-14 12:53:36 +0100

    Buffer solution directe

--- src/jus/poc/prodcons/v1/ProdConsBuffer.java
+++ src/jus/poc/prodcons/v1/ProdConsBuffer.java
@@ -2,36 +2,56 @@ package jus.poc.prodcons.v1;
 
 public class ProdConsBuffer implements IProdConsBuffer {
 
-	int buffer_size = 10;
-
+	int buffer_size;
 	Message[] Buffer = new Message[buffer_size];
-	int index_buff = 0;
+	int index_buff = -1;
+	int total_msgs = 0;
 
-	@Override
-	public void put(Message m) throws InterruptedException {
-		if (index_buff >= buffer_size) {
-			wait();
-		}
-		Buffer[index_buff] = m;
-		index_buff++;
+	public ProdConsBuffer(int buffer_size) {
+		this.buffer_size = buffer_size;
 	}
 
 	@Override
-	public Message get() throws InterruptedException {
-		// TODO Auto-generated method stub
-		return null;
+	public void put(Message m) {
+		while (index_buff >= buffer_size - 1) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		Buffer[index_buff + 1] = m;
+		index_buff++;
+		total_msgs++;
+		notifyAll();
+	}
+
+	@Override
+	public Message get() {
+		Message m;
+		while (nmsg() == 0) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		m = Buffer[index_buff];
+		index_buff--;
+		notifyAll();
+		return m;
 	}
 
 	@Override
 	public int nmsg() {
-		// TODO Auto-generated method stub
-		return 0;
+		return index_buff + 1;
 	}
-	
+
 	@Override
 	public int totmsg() {
-		// TODO Auto-generated method stub
-		return 0;
-	}	
+		return total_msgs;
+	}
+
+	public void setBuffer_size(int buffer_size) {
+		this.buffer_size = buffer_size;
+	}
 
 }
